aa0ac98e3c3512e272b2dc539ea9114c7e97cd9e
Joshua Jung <mail@joshua-jung.com>
2020-04-07 18:20:23 +0200

    Set a cookie when signing in

--- web/src/store/substores/AuthStore.ts
+++ web/src/store/substores/AuthStore.ts
@@ -4,6 +4,7 @@ import { RootStore, IRootStore } from "../RootStore";
 import { ISignInFormData } from "../models/SignInFormData";
 import { AxiosResponse } from "axios";
 import { SignInPostResponseDto } from "../../shared/dtos/signIn.post.response.dto";
+import * as Nookies from "nookies";
 
 export const AuthStore = types
   .model({ sessionToken: types.maybe(types.string) })
@@ -19,9 +20,14 @@ export const AuthStore = types
     // Public
     const setToken = (token: string) => {
       self.sessionToken = token;
+      Nookies.setCookie(null, "wljwt", token, {
+        maxAge: 365 * 24 * 60 * 60,
+        path: "/",
+      });
     };
     const unsetToken = () => {
       self.sessionToken = undefined;
+      Nookies.destroyCookie(null, "wljwt", {});
     };
     const signIn = flow(function* (data: ISignInFormData) {
       const result: AxiosResponse<SignInPostResponseDto> = yield apiService.post(
